107667d32d3280b3107cf3b925f76cde39355d14
Itamar1567 <Itamarbaharav@gmail.com>
2025-07-16 17:49:22 -0400

    Basic Settings
    Added a basic settings window

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -31,6 +31,6 @@ public class PauseMenu : MonoBehaviour
 
     public void Settings()
     {
-
+        UIControl.Singleton.OpenWidow(4);
     }
 }
---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Controlls()
+    {
+        UIControl.Singleton.OpenWidow(1);
+    }
+    public void Sound()
+    {
+        UIControl.Singleton.OpenWidow(1);
+    }
+    public void Return()
+    {
+        UIControl.Singleton.OpenWidow(3);
+    }
+}
+
